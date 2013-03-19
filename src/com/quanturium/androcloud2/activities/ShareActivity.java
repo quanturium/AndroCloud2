7f26bc6ab18e0e09a13a57c33e99f6498a21a4ad
Arnaud Frugier <frugier.arnaud@gmail.com>
2013-03-18 21:28:59 -0700

    Added condition before sharing a file

--- src/com/quanturium/androcloud2/activities/ShareActivity.java
+++ src/com/quanturium/androcloud2/activities/ShareActivity.java
@@ -3,8 +3,11 @@ package com.quanturium.androcloud2.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.quanturium.androcloud2.MyApplication;
+import com.quanturium.androcloud2.tools.Prefs;
+import com.quanturium.androcloud2.tools.Tools;
 
 public class ShareActivity extends Activity
 {
@@ -18,7 +21,18 @@ public class ShareActivity extends Activity
 		Intent intent = getIntent();
 		
 		if(intent != null)
-			((MyApplication)getApplication()).sendToMainService(intent);
+		{
+			if(Prefs.getPreferences(this).getBoolean(Prefs.LOGGED_IN, false))
+			{
+				((MyApplication)getApplication()).sendToMainService(intent);
+			}
+			else
+			{
+				Toast.makeText(this, "You need to be logged in with your CloudApp account to upload a file", Toast.LENGTH_LONG).show();
+				startActivity(new Intent(this, MainActivity.class));
+			}			
+		}
+			
 		
 		finish();
 	}
